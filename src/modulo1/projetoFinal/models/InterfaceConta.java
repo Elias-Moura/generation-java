5524102e82babf383c710ec688e7f7fee7308019
Elias Moura <elias.smourayt@gmail.com>
2023-09-14 17:19:41 -0300

    sacar também levanta exception.

--- src/modulo1/projetoFinal/models/InterfaceConta.java
+++ src/modulo1/projetoFinal/models/InterfaceConta.java
@@ -1,9 +1,9 @@
 package modulo1.projetoFinal.models;
 
-import modulo1.projetoFinal.exception.OperacaoDeDepositoInvalida;
+import modulo1.projetoFinal.exception.TransacaoInvalidaException;
 
 public interface InterfaceConta {
-    public double sacar(double valor);
-    public void depositar(double valor) throws OperacaoDeDepositoInvalida;
+    public void sacar(double valor) throws TransacaoInvalidaException;
+    public void depositar(double valor) throws TransacaoInvalidaException;
     public double getSaldo();
 }
